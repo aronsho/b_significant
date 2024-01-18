ebc75b05cbc2f423d54ab990785fcdfc6cae52e3
Aron Mirwald <aron.mirwald@sed.ethz.ch>
2024-01-18 11:16:34 +0100

    add simulate_magnitudes function

--- functions/general_functions.py
+++ functions/general_functions.py
@@ -1,5 +1,6 @@
 # imports
 import numpy as np
+from seismostats import simulate_magnitudes, bin_to_precision
 
 
 def transform_n(x: float, b: float, n1: int, n2: int):
@@ -38,3 +39,39 @@ def acf_lag_n(series: np.ndarray, lag: int = 1):
         # print(len(b_series), np.mean(b_series), "len, mean")
         acf /= sum((series - np.mean(series)) ** 2)
     return acf
+
+
+def simulated_magnitudes_binned(
+    n: int,
+    b: float,
+    mc: float,
+    delta_m: float,
+    mag_max: float = None,
+    b_parameter: str = "b_value",
+) -> np.ndarray:
+    """simulate magnitudes and bin them to a given precision. input 'b' can be
+    specified to be beta or the b-value, depending on the 'b_parameter' input
+
+    Args:
+        n:              number of magnitudes to simulate
+        b:              b-value or beta of the distribution from which
+                magnitudes are simulated
+        mc:             completeness magnitude
+        delta_m:        magnitude bin width
+        mag_max:        maximum magnitude
+        b_parameter:    'b_value' or 'beta'
+
+    Returns:
+        mags:   array of magnitudes
+    """
+    if b_parameter == "b_value":
+        beta = b * np.log(10)
+    elif b_parameter == "beta":
+        beta = b
+    else:
+        raise ValueError("b_parameter must be 'b_value' or 'beta'")
+
+    mags = simulate_magnitudes(n, beta, mc - delta_m / 2, mag_max)
+    if delta_m > 0:
+        mags = bin_to_precision(mags, delta_m)
+    return mags
