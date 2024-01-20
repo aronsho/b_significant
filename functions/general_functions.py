46addf7770e656f403a3967873f14fed87b3e534
Aron Mirwald <aron.mirwald@sed.ethz.ch>
2024-01-20 15:15:22 -0600

    added utsu test and simulate_step

--- functions/general_functions.py
+++ functions/general_functions.py
@@ -43,7 +43,7 @@ def acf_lag_n(series: np.ndarray, lag: int = 1):
 
 def simulated_magnitudes_binned(
     n: int,
-    b: float,
+    b: float | np.ndarray,
     mc: float,
     delta_m: float,
     mag_max: float = None,
@@ -55,7 +55,9 @@ def simulated_magnitudes_binned(
     Args:
         n:              number of magnitudes to simulate
         b:              b-value or beta of the distribution from which
-                magnitudes are simulated
+                magnitudes are simulated. If b is np.ndarray, it must have the
+                length n. Then each magnitude is simulated from the
+                corresponding b-value
         mc:             completeness magnitude
         delta_m:        magnitude bin width
         mag_max:        maximum magnitude
@@ -75,3 +77,64 @@ def simulated_magnitudes_binned(
     if delta_m > 0:
         mags = bin_to_precision(mags, delta_m)
     return mags
+
+
+def simulate_step(
+    n: int,
+    n_deviation: int,
+    b: float,
+    delta_b: float,
+    mc: float,
+    delta_m: float = 0.01,
+):
+    """Simulate binned magnitudes with a step of length N_deviation in the
+    b-value
+
+    Args:
+        n:              total number of magnitudes to simulate
+        n_deviation:    number of magnitudes with deviating b-value
+        b:              b-value of the background
+        delta_b:        deviation of b-value
+        mc:             completeness magnitude
+        delta_m:        magnitude bin width
+
+    Returns:
+        magnitudes:   array of magnitudes
+        b_true: array of b-values from which each magnitude was simulated
+
+    """
+    n_loop1 = int((n - n_deviation) / 2)
+
+    b_true = np.ones(n) * b
+    b_true[n_loop1 : n_loop1 + n_deviation] = b + delta_b  # noqa
+
+    magnitudes = simulated_magnitudes_binned(n, b, mc, delta_m)
+
+    return magnitudes, b_true
+
+
+def utsu_test(b1: float, b2: float, n1: int, n2: int):
+    """Given two b-value estimates from two magnitude samples, this functions
+    gives back the probability that the actual underlying b-values are not
+    different. A small p-value means that the b-values are
+
+    Source: TODO Need to verify that this is used in Utsu 1992 !!!
+
+    Args:
+        b1:     b-value estimate of first sample
+        b2:     b-value estimate of seconds sample
+        N1:     number of magnitudes in first sample
+        N2:     number of magnitudes in second sample
+
+    Returns:
+        p:      Probability that the underlying b-value of the two samples is
+            identical
+    """
+    delta_AIC = (
+        -2 * (n1 + n2) * np.log(n1 + n2)
+        + 2 * n1 * np.log(n1 + n2 * b1 / b2)
+        + 2 * n2 * np.log(n2 + n1 * b2 / b1)
+        - 2
+    )
+    p = np.exp(-delta_AIC / 2 - 2)
+    return p
